47e92776c017cbf5870f7b9b77f309d057ce335a
Mind Prisoned <yarinkash1@gmail.com>
2025-11-25 22:45:57 +0200

    Fix whitespace formatting for WallClimber.cs

--- Assets/Scripts/3-collisions/WallClimber.cs
+++ Assets/Scripts/3-collisions/WallClimber.cs
@@ -5,21 +5,26 @@ using UnityEngine;
 public class WallClimber : MonoBehaviour
 {
     [Tooltip("Direction and speed of movement, in units per second")]
-    [SerializeField] Vector3 speed;
+    [SerializeField]
+    private Vector3 speed;
 
-    void Update() {
+    private void Update()
+    {
         transform.position += speed * Time.deltaTime;
     }
 
-    private void OnCollisionEnter2D(Collision2D collision) {
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
         RotateSpeed90Degrees();
     }
 
-    private void OnTriggerEnter2D(Collider2D collision) {
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
         RotateSpeed90Degrees();
     }
 
-    private void RotateSpeed90Degrees() {
+    private void RotateSpeed90Degrees()
+    {
         Quaternion rotation = Quaternion.Euler(0, 0, 90);   // 90 degrees leftwards (around Z axis)
         speed = rotation * speed;       // rotate the speed vector
     }
